ebc0377b2cdb10c11370265f683fdd4d94ae8035
Ryan Shankar <shankary@oregonstate.edu>
2026-06-14 21:15:14 -0700

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,5 @@
+# The food item class takes 4 things: The name of a food, the cost of it, the category of the food, and how popular the food item is.
+# For the Customer class the name of the customer, their purchase history, and that the user is actually signed up.
+# The Menu class takes the items as a list, adding items to the list, and a filter method.
+# Order class is their to calculate cost and confirm that the order is sent to the correct customer.
+  
